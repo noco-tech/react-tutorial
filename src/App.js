c40b40312b78cbf14f0798a19f2fa81903886eeb
sadainoaraisan <1991000012rg@cyber-u.ac.jp>
2022-03-29 08:02:42 +0900

    hello

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import {List} from './List'
+
+function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+  const changeDescription = () => {
+    setDescription('クリック後の表示')
+  }
+  return (
+    <div>
+      {description}
+      <List title="取り扱い言語一覧" list="python" />
+      <button onClick={changeDescription}>ボタン</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,13 @@
+
+export const List = ({title, list}) => {
+    return (
+        <div>
+
+            <h4>{title}</h4>
+            <p>{list}</p>
+            <div>リストです</div>
+        </div>
+      
+    )
+    
+}
