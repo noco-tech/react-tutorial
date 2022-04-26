ee9c7da913f12dde41d2d7a5a775cb3d2a2775b1
sadainoaraisan <1991000012rg@cyber-u.ac.jp>
2022-04-26 22:04:34 +0900

    #6 main-branch

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import {useState} from 'react'
-import { List } from './List';
-import {Form} from './Form'
+import { useState } from "react";
+import { List } from "./List";
+import { Form } from "./Form";
+import { LANGUAGES } from "./const/language";
 
 function App() {
-  
-  const [tab, setTab] = useState('list');
+  const [tab, setTab] = useState("list");
+  const [langs, setLangs] = useState(LANGUAGES);
 
-    return(
-      <div>
-        <header>
-          <ul>
-            <li onClick={() => setTab('list')}>リスト</li>
-            <li onClick={() => setTab('form')}>フォーム</li>
-          </ul>
-        </header>
-        <hr/>
-        
-        {
-          tab === 'list' ? <List /> : <Form />
-        }
-        
-      </div>
-    )
+  const addLang = (lang) => {
+    setLangs([...langs, lang]);
+    setTab("list");
+  };
+
+  return (
+    <div>
+      <header>
+        <ul>
+          <li onClick={() => setTab("list")}>リスト</li>
+          <li onClick={() => setTab("form")}>フォーム</li>
+        </ul>
+      </header>
+      <hr />
+
+      {tab === "list" ? <List langs={langs} /> : <Form onAddLang={addLang} />}
+    </div>
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,26 +1,11 @@
-const LANGUAGES = [
-    'JavaScript',
-    'C++',
-    'Ruby',
-    'Java',
-    'PHP',
-    'Go',
-];
+export const List = ({ langs }) => {
+  return (
+    <div>
+      <h4></h4>
 
-export const List = () => {
-    
-        return (
-            <div>
-    
-                <h4></h4>
-                
-                {
-                    LANGUAGES.map((lang, index) => {
-                      return <div key={index}>{lang}</div> 
-                    })
-                }
-            </div>
-          
-        )
-    
-}
+      {langs.map((lang, index) => {
+        return <div key={index}>{lang}</div>;
+      })}
+    </div>
+  );
+};
